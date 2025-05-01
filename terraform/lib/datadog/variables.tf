39c20ebc8f7bfb6ad1b9abcd097dd51b4e00bc38
jasonmimick-aws <mimjasov@amazon.com>
2025-05-01 18:52:23 -0400

    Fix duplicate variable declarations in Datadog module

--- terraform/lib/datadog/variables.tf
+++ terraform/lib/datadog/variables.tf
@@ -1,16 +1,2 @@
-variable "vpc_id" {
-  description = "ID of the VPC"
-  type        = string
-}
-
-variable "catalog_security_group_id" {
-  description = "Security group ID for the catalog database"
-  type        = string
-  default     = ""
-}
-
-variable "orders_security_group_id" {
-  description = "Security group ID for the orders database"
-  type        = string
-  default     = ""
-}
+# This file is intentionally left empty.
+# All variables are now defined in database_monitoring.tf to avoid duplication.
