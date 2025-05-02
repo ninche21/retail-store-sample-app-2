c7b782b366620c537627b35e4751943e0aef1a85
Nina Chen <nina@ninas-air.mynetworksettings.com>
2025-05-02 19:41:55 -0400

    fix: remove duplicate http prefix in application_url output

--- terraform/ecs/default/output.tf
+++ terraform/ecs/default/output.tf
@@ -1,6 +1,6 @@
 output "application_url" {
   description = "URL where the application can be accessed"
-  value       = "http://${module.retail_app_ecs.ui_service_url}"
+  value       = module.retail_app_ecs.ui_service_url
 }
 
 output "environment_name" {
